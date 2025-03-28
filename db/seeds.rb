4299ecd8c1b1d77e2f9bf0791b35bc96e09ce347
sandhra <sandhrasonabijoy@gmail.com>
2025-03-28 10:16:21 +0900

    added the find or create by in seeds under db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.find_or_create_by(email_address: "admin@test.com", password: "admin123", role: :admin)
