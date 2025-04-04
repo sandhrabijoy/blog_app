f39e5710d7a282758b1b76ce03588513049a237a
sandhra <sandhrasonabijoy@gmail.com>
2025-04-04 13:24:02 +0900

    created files to edit post in blog

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class PostsController < ApplicationController
     before_action :require_authentication, :require_admin
+    before_action :set_post[:edit, :update, :destroy]
 
     def new
       @post = Post.new
@@ -15,7 +16,17 @@ module Admin
       end
     end
 
+    def edit
+    end
+    def update
+    end
+    def destroy
+    end
+
     private
+    def set_post
+      @post= Post.friendly.find(params[:id])
+    end
 
     def require_admin
       unless Current.session&.user&.admin?
