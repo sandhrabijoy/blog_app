620d0d2c646d86e4bd2aa178f0162218d9cf6364
sandhra <sandhrasonabijoy@gmail.com>
2025-04-04 09:41:06 +0900

    banner image field added with banner_image,condition added to have banner_image else to upload default image

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,17 +1,31 @@
 module Admin
   class PostsController < ApplicationController
     before_action :require_authentication, :require_admin
+
     def new
-      @post= Post.new
-    end
-    def create
+      @post = Post.new
     end
 
-  private
-  def require_admin
-    if !Current.session&.user&.admin?
-      redirect_to root_path, alert: "You are not authorized to view this page"
+    def create
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to @post, notice: "Post was successfully created"
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def require_admin
+      unless Current.session&.user&.admin?
+        redirect_to root_path, alert: "You are not authorized to view this page"
+      end
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body, :banner_image)
     end
   end
-  end
 end
+
