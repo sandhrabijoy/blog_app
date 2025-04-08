b31ddd271843be43ccdf14a9496b663f87d75071
sandhra <sandhrasonabijoy@gmail.com>
2025-04-08 10:24:07 +0900

    added validation of title field

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   friendly_id :title, use: :slugged
   has_one_attached :banner_image
   has_rich_text :body
+
+  validates :title, presence: { message: "Title cannot be empty" }
 end
