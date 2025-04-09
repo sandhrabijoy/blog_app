d9ddd74da95297d0e8fb4a438b52930e6a193359
sandhra <sandhrasonabijoy@gmail.com>
2025-04-09 17:28:13 +0900

    the bullet points for numbered and unordered started working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
+  config.assets.debug = true
 
   # Do not eager load code on boot.
   config.eager_load = false
