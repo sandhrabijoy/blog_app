84c4631553238abf5e23d842cb0f6e91a1f2f249
sandhra <sandhrasonabijoy@gmail.com>
2025-04-11 13:38:59 +0900

    toggle sorted and everything workiing well

---
+++ app/controllers/dark_mode_controller.rb
@@ -0,0 +1,6 @@
+class DarkModeController < ApplicationController
+    def create
+      Current.user.update(dark_mode: !Current.user.dark_mode)
+      redirect_back(fallback_location: root_path)
+    end
+  end
